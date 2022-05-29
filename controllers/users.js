89d55647fb083929db520a1748f31a2481d1d5e9
dvsolodov <d.v.solodov@yandex.ru>
2022-05-29 17:27:56 +1000

    feat: add request data checking

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,13 @@ const { handleErrors } = require('../utils/utils');
 
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
+    .then((users) => {
+      if (!users) {
+        res.status(404).send({ message: 'Данные не найдены' });
+      }
+
+      res.send({ data: users });
+    })
     .catch((err) => handleErrors(err, res));
 };
 
@@ -29,7 +35,13 @@ const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'Данные не найдены' });
+      }
+
+      res.send({ data: user });
+    })
     .catch((err) => handleErrors(err, res));
 };
 
@@ -58,13 +70,24 @@ const updateUser = (req, res) => {
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
+  const userId = req.user._id;
+
+  if (!userId.match(/^[\w\d]{24}$/)) {
+    res.status(400).send({ message: 'Переданы некорректные данные' });
+  }
 
   User.findByIdAndUpdate(
-    req.user._id,
+    userId,
     { avatar },
     { new: true, runValidators: true, upsert: true },
   )
-    .then((user) => res.send({ data: user }))
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'Данные не найдены' });
+      }
+
+      res.send({ data: user });
+    })
     .catch((err) => handleErrors(err, res));
 };
 
