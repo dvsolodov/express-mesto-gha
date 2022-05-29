db92cf8fe0a8398545f0f55c0aba3c18451c4799
dvsolodov <d.v.solodov@yandex.ru>
2022-05-30 00:18:35 +1000

    hotfix: improve 404 handling

--- routes/404.js
+++ routes/404.js
@@ -3,10 +3,6 @@ const { handle404 } = require('../controllers/404');
 
 const router = express.Router();
 
-router.get('*', handle404);
-router.post('*', handle404);
-router.put('*', handle404);
-router.patch('*', handle404);
-router.delete('*', handle404);
+router.use((req, res) => handle404(req, res));
 
 module.exports = router;
