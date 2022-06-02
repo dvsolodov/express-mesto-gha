24fd02a69686d3b25502528486085715a75cab0f
dvsolodov <d.v.solodov@yandex.ru>
2022-06-02 22:42:19 +1000

    feat: add email and password to User model

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const { handleErrors } = require('../utils/utils');
 const { ERR_400, ERR_404 } = require('../utils/constants');
@@ -39,9 +40,10 @@ const getUserById = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const { name, about, avatar, email, password } = req.body;
 
-  User.create({ name, about, avatar })
+  bcrypt.hash(password, 10)
+    .then(hash => User.create({ name, about, avatar, email, password: hash })
     .then((user) => {
       if (!user) {
         res.status(ERR_404).send({ message: 'Данные не найдены' });
