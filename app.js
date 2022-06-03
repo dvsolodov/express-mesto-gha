dc41c21d9f1a9aad34645ff2570ea4a868c931f7
dvsolodov <d.v.solodov@yandex.ru>
2022-06-03 23:27:31 +1000

    feat: remove user id hardcore in app.js

--- app.js
+++ app.js
@@ -21,15 +21,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-/*
-app.use((req, res, next) => {
-  req.user = {
-    _id: '628b5cbd790c2e7b20a054ee',
-  };
-
-  next();
-});
-*/
 app.use(bodyParser.json());
 app.use(cookieParser());
 
