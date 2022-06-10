5e623057707da9d324fc245cae3a4d23fb2a892d
dvsolodov <d.v.solodov@yandex.ru>
2022-06-10 16:28:22 +1000

    hotfix: remove pattern validation from routes

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const auth = require('./middlewares/auth');
 const { PORT = 3000 } = process.env;
 const {
   urlPattern,
-  passwordPattern,
 } = require('./utils/constants');
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -34,14 +33,14 @@ app.use(cookieParser());
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().pattern(passwordPattern),
+    password: Joi.string().required(),
   }),
 }), login);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().pattern(passwordPattern),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(urlPattern),
